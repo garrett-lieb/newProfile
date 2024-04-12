a157595a3a6e7b27247f30bb9585e838d74673b5
garrett lieb <liebg08@gmail.com>
2024-04-11 17:39:21 -0700

    added functionality to nav items container to hide under more conditions

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -4,16 +4,25 @@ import "./nav.css"
 import { useState } from 'react';
 
 const Root = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-        // if you click away close the dropBar
-        document.addEventListener("click", (e) => {
-            if (e.target.className !== "menuButton") {
-                setIsMenuOpen(false);
-            }
-        });
-    };
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+    // if user clicks on the menu button, the menu will open and close
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+    // if the menu is open and the user clciks somewhere on the page the menu will close
+    window.onclick = function(event) {
+        if (!event.target.matches('.menuButton')) {
+            setIsMenuOpen(false);
+        }
+    }
+    // if the user scrolls down 50px the menu will close
+    window.onscroll = function() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            setIsMenuOpen(false);
+        }
+    }
+    
+    
+
+    
 
     return (
         <div class="menu">
