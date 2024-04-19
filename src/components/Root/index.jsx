0c40fd9af5ff822aa9612d12086b8e4532ad1f4c
garrett lieb <liebg08@gmail.com>
2024-04-19 02:16:32 -0700

    revert from accidental branch merge

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import {Link, Outlet} from 'react-router-dom'
 import "./nav.css"
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const Root = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(true);
-    // if user clicks on the menu button, the menu will open and close
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-    // if the menu is open and the user clciks somewhere on the page the menu will close
-    window.onclick = function(event) {
-        if (!event.target.matches('.menuButton')) {
-            setIsMenuOpen(false);
-        }
-    }
-    // if the user scrolls down 50px the menu will close
-    window.onscroll = function() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            setIsMenuOpen(false);
-        }
-    }
+    // const [isMenuOpen, setIsMenuOpen] = useState(true);
+    // // if user clicks on the menu button, the menu will open and close
+    // const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+    // // if the menu is open and the user clciks somewhere on the page the menu will close
+    // window.onclick = function(event) {
+    //     if (!event.target.matches('.menuButton')) {
+    //         setIsMenuOpen(false);
+    //     }
+    // }
+    // // if the user scrolls down 50px the menu will close
+    // window.onscroll = function() {
+    //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //         setIsMenuOpen(false);
+    //     }
+    // }
+    
+    
+
     
 
     return (
         <div className="menu">
-            <button className="menuButton" onClick={toggleMenu}>
+            {/* <button className="menuButton" onClick={toggleMenu}>
                 Menu
             </button>
   
-            {isMenuOpen && (
+            {isMenuOpen && ( */}
                 <div className="navBar">
                 <ul className="dropBar">
                     <li>
@@ -53,7 +56,8 @@ const Root = () => {
                     </li>
                 </ul>
                 </div>
-            )}
+            {/* )} */}
+  
             <Outlet />
         </div>
     );
